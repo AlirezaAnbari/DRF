f725fee08e1d3ca1471b23de611bb56cbfb89f0f
Alireza Anbari <alireza.anbary@gmail.com>
2024-01-16 12:31:01 +0330

    UserManager
    -Create UserManager
    - config setting.py to use our custom user model.
    -create superuer(admin@admin.com)

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,38 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, PermissionsMixin)
 
 # Create your models here.
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    # is_verified = models.BooleanField(default=False)
-    firsr_name = models.CharField(max_length=20)
-    
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-    
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.email
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
 class Post(models.Model):
     '''
     Define posts for blog app
--- core/core/settings.py
+++ core/core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'blog',
+    # 'blog',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# user manager config
+AUTH_USER_MODEL = "accounts.User"
