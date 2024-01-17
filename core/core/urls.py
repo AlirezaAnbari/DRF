ae1292080738d63216e5b44f6c8534cc0b763549
Alireza Anbari <alireza.anbary@gmail.com>
2024-01-17 12:24:26 +0330

    accounts:admin / profile class
    create a admin of accounts.
    create a Proile table and connected to User table with signal.
    create STATIC&MEDIA dir in my project

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# This is not suitable for production use! 
+# Serving static and media for development
+if settings.DEUBG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+
