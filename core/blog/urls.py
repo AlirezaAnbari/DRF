b692dac750d8bb109ab8bc37b3d40e22f2846329
Alireza Anbari <alireza.anbary@gmail.com>
2024-01-18 13:15:01 +0330

    start class based view

---
+++ core/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+urlpatterns = [
+    path('fbv-index', views.indexView, name='fbv-index'),      
+    # path('cbv-index', TemplateView.as_view(template_name='index.html')),   
+    path('cbv-index', views.IndexView.as_view(), name='cbv-index')
+]
