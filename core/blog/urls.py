67766da73b6cb1043856c78e118860e0af2bc6b8
Alireza Anbari <alireza.anbary@gmail.com>
2024-01-19 18:28:59 +0330

    TemplateView-RedirectView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from . import views
+
+app_name = 'blog'
 
 urlpatterns = [
     path('fbv-index', views.indexView, name='fbv-index'),      
     # path('cbv-index', TemplateView.as_view(template_name='index.html')),   
-    path('cbv-index', views.IndexView.as_view(), name='cbv-index')
+    path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
+    path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index'), name='go-to-index'),
 ]
