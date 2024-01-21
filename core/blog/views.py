56abeb26f482e2c32d7d2ae0939b180fa3048339
Alireza Anbari <alireza.anbary@gmail.com>
2024-01-21 19:28:18 +0330

    CreateView,UpdateView,DeleteView and PermissionMixins

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, RedirectView
-from django.views.generic import ListView, DetailView, FormView
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # Create your views here.
 def indexView(request):
@@ -10,6 +11,7 @@ def indexView(request):
     a function based view to show index page
     '''
     return render(request, "index.html")
+
 
 class IndexView(TemplateView):
     '''
@@ -24,16 +26,20 @@ class IndexView(TemplateView):
         context['posts'] = Post.objects.all()
         return context
     
-class PostListView(ListView):
+    
+class PostListView(PermissionRequiredMixin, LoginRequiredMixin, ListView):
+    permission_required = 'blog.view_post'
     # model = Post
     queryset = Post.objects.all()
-    context_object_name = 'posts'
     # paginate_by = 2
+    context_object_name = 'posts'
     ordering = '-id'
     
-class PostDetailView(DetailView):
-    model = Post
     
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+
+'''
 class PostCreateView(FormView):
     template_name = 'contact.html'
     form_class = PostForm
@@ -42,3 +48,27 @@ class PostCreateView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+'''
+
+class PostCreateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    # fields = ['author','title','content',
+    #           'status','category','published_date']
+    form_class = PostForm
+    success_url = '/blog/post/'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super(PostCreateView, self).form_valid(form)
+    
+    
+class PostEditView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/' 
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = '/blog/post/'
+    
+    
