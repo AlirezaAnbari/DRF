499571fe2dac9ae13aed402e85c6bddfe87963a7
Alireza Anbari <alireza.anbary@gmail.com>
2024-02-05 13:00:23 +0330

    testing with UnitTest
    -Models
    -urls
    -forms
    -models
    -views
    added testing with PyTest to a  .zip file called unit_tests.zip

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -5,15 +5,12 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    # path('fbv-index', views.indexView, name='fbv-index'),
-    # path('cbv-index', TemplateView.as_view(template_name='index.html')),
-    # path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
-    # path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index'), name='go-to-index'),
+    path('', views.IndexView.as_view(), name='index'),
     path("post/", views.PostListView.as_view(), name="post-list"),
     path(
         "post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"
     ),
-    # path('post/create/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/create/', views.PostCreateView.as_view(), name='post-create'),
     path(
         "post/<int:pk>/edit/", views.PostEditView.as_view(), name="post-edit"
     ),
